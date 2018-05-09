4e823f151d275798f15e7f9bd036fc751918a97d
Bernd Porr <mail@berndporr.me.uk>
2018-05-09 23:20:29 +0100

    Updated the setup script so that it's able to create a source distro

--- cpp/setup.py
+++ cpp/setup.py
@@ -14,9 +14,12 @@ attyscomm_module = Extension('_pyattyscomm',
                            )
 
 setup (name = 'pyattyscomm',
-       version = '1.0',
-       author      = "Glasgow Neuro LTD",
-       description = """AttysComm""",
+       version = '1.2.1',
+       author      = "Bernd Porr",
+       author_email = "bernd@glasgowneuro.tech",
+       url = "https://github.com/glasgowneuro/AttysComm",
+       license = "../LICENSE",
+       description = """API for the Attys (www.attys.tech) under Linux""",
        ext_modules = [attyscomm_module],
        py_modules = ["pyattyscomm"],
        )
