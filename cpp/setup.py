0ce4d7956a88a4fa1b8f6c1eed3c63ac4b911c86
Bernd Porr <mail@berndporr.me.uk>
2018-05-11 17:57:06 +0100

    Made link more prominent

--- cpp/setup.py
+++ cpp/setup.py
@@ -17,7 +17,7 @@ attyscomm_module = Extension('_pyattyscomm',
                            )
 
 setup (name = 'pyattyscomm',
-       version = '1.2.1b18',
+       version = '1.2.1b19',
        author      = "Bernd Porr",
        author_email = "bernd@glasgowneuro.tech",
        url = "https://github.com/glasgowneuro/AttysComm",
