20a493ea1eedc4f41946f2102aecfcc985ace826
Bernd Porr <mail@berndporr.me.uk>
2020-01-15 11:26:55 +0000

    Added a "connect" message for debugging.

--- cpp/AttysScan.cpp
+++ cpp/AttysScan.cpp
@@ -99,6 +99,7 @@ int AttysScan::scan(int maxAttysDevs) {
 					attysComm[nAttysDevices]->connect();
 					strncpy(attysName[nAttysDevices],name,sizeof(name));
 					nAttysDevices++;
+					fprintf(stderr," Connected.\n");
 					break;
 				} 
 				catch (const char *msg) {
@@ -108,6 +109,7 @@ int AttysScan::scan(int maxAttysDevs) {
 					attysComm[nAttysDevices]->closeSocket();
 					delete attysComm[nAttysDevices];
 					attysComm[nAttysDevices] = NULL;
+					fprintf(stderr," Connection error.\n");
 				}
 			} else {
 				fprintf(stderr,"\n");
